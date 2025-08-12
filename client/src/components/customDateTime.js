7e8ede8faf6753438cfd5daa6289f6ea1da76f0b
AKovalysko <57531154+Akaki411@users.noreply.github.com>
2025-08-12 06:50:56 +0500

    Delete webpack files

--- client/src/components/customDateTime.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const CustomDateTime = (props) => {
-    return (
-        <input style={{height: 38, display: props.disable ? "none" : "block"}} type="datetime-local" className="admin-new-article_select" onChange={key => props.onChange(new Date(key.target.value))}/>
-    );
-};
-
-export default CustomDateTime;
